809c74a5e914ef2cae87a6d1f7bfccb46dd65a4f
Ezequiel Agustin Ibarra <82052522+ezeagusibarra001@users.noreply.github.com>
2023-07-30 03:25:31 -0300

    Icons

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -7,7 +7,8 @@ export default function Icon({ name, color }: IconProps) {
     const iconDict: { [T in IconName]: JSX.Element } = {
         hamburger: (
             <>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="16" viewBox="0 0 24 16" fill="none">
+                <svg xmlns="http://www.w3.org/2000/svg" width="100%"
+                    height="100%" viewBox="0 0 24 16" fill="none">
                     <path d="M1 1H23" stroke-width="2" stroke-linecap="round" />
                     <path d="M1 8H23" stroke-width="2" stroke-linecap="round" />
                     <path d="M1 15H23" stroke-width="2" stroke-linecap="round" />
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as Layout } from "./Layout";
+export { default as Icon } from "./Icon";
 export { default as CssBaseline } from "./CssBaseline";
