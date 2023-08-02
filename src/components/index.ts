30141bbd240a909c18333b9af1526268f5d21afe
Ezequiel Agustin Ibarra <82052522+ezeagusibarra001@users.noreply.github.com>
2023-08-02 01:26:45 -0300

    f

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button";
 export { default as Layout } from "./Layout";
 export { default as Icon } from "./Icon";
+export { default as Card } from "./Card";
 export { default as CssBaseline } from "./CssBaseline";
