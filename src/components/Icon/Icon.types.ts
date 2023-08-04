2b983dfa3d7c997dd2ef69c5ae094e6c7816491e
Ezequiel Agustin Ibarra <82052522+ezeagusibarra001@users.noreply.github.com>
2023-08-04 17:36:52 -0300

    nashe

--- src/components/Icon/Icon.types.ts
+++ src/components/Icon/Icon.types.ts
@@ -1,19 +1,23 @@
 import { Color, SHADES } from "../../types/types";
 
 export const IconNames = [
-   "hamburger",
-   "telegram",
-   "instagram",
-   "youtube",
-   "facebook",
-   "tiktok",
-   "cross"
+  "hamburger",
+  "telegram",
+  "instagram",
+  "youtube",
+  "facebook",
+  "tiktok",
+  "cross",
+  "course",
+  "clip",
+  "play",
+  "stars",
 ] as const;
 
-export type IconName = typeof IconNames[number];
+export type IconName = (typeof IconNames)[number];
 
 export type IconProps = {
-    name: IconName;
-    color?: Color;
-    shade?: SHADES;
+  name: IconName;
+  color?: Color;
+  shade?: SHADES;
 };
