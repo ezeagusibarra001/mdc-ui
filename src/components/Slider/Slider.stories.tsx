cc6436b8b8cc06723862cdc130fecb5dee6c46b2
Ezequiel Agustin Ibarra <ezequielibarra@vexels.com>
2023-08-24 01:20:54 -0300

    Slider

--- src/components/Slider/Slider.stories.tsx
+++ src/components/Slider/Slider.stories.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Slider from '.'
@@ -15,6 +16,12 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
     args: {
-
+        items: [
+            <div>1</div>,
+            <div>2</div>,
+            <div>3</div>,
+            <div>4</div>,
+            <div>5</div>,
+        ],
     },
 };
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Autoplay, Pagination } from 'swiper/modules';
 
-export default function Slider() {
+type SliderProps = {
+    items: React.ReactNode[];
+    loop?: boolean;
+    delay?: number;
+}
+
+export default function Slider({ items, loop = true, delay = 2500 }: SliderProps) {
     return (
-        <div>Slider</div>
-    )
+        <Swiper
+            autoplay={{
+                delay: delay,
+                disableOnInteraction: false,
+            }}
+            pagination={{
+                clickable: true,
+                dynamicBullets: true,
+            }}
+            className='carousel'
+            loop={loop}
+            modules={[Autoplay, Pagination]}
+        >
+            {items.map((item, index) => (
+                <SwiperSlide key={index}>{item}</SwiperSlide>
+            ))}
+        </Swiper>
+    );
 }
