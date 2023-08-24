5e775b227410a900dde0c3383b58306a4dfa0109
Ezequiel Agustin Ibarra <ezequielibarra@vexels.com>
2023-08-24 01:24:03 -0300

    fix export slider

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export { default as Layout } from "./Layout";
 export { default as Icon } from "./Icon";
 export { default as Card } from "./Card";
 export { default as Input } from "./Input";
+export { default as Slider } from "./Slider";
 export { default as CssBaseline } from "./CssBaseline";
