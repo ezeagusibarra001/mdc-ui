c57f20979d435a636f6a911f51e21358a2abcc5e
ezeagusibarra001 <ibarra.ezequiel01@gmail.com>
2023-10-23 12:01:28 -0300

    fixes

--- src/components/Layout/Drawer.tsx
+++ src/components/Layout/Drawer.tsx
@@ -3,13 +3,13 @@ import Icon from "../Icon/Icon";
 import { HeaderProps } from "./Header";
 import classNames from "./Layout.module.css";
 
-type MobileDraweProps = HeaderProps
+type MobileDraweProps = HeaderProps;
 
 const MobileDrawer = ({
   color = "black",
   shade = "DEFAULT",
   logo,
-  menu
+  menu,
 }: MobileDraweProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -18,19 +18,23 @@ const MobileDrawer = ({
   };
 
   const useWidth = () => {
-    const [width, setWidth] = useState(0)
-    const handleResize = () => setWidth(window.innerWidth)
+    const [width, setWidth] = useState(0);
+    const handleResize = () => setWidth(window.innerWidth);
     useEffect(() => {
-      handleResize()
-      window.addEventListener('resize', handleResize)
-      return () => window.removeEventListener('resize', handleResize)
+      handleResize();
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
       // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-    return width
-  }
-  const windowWidth = useWidth()
-  const closeDrawerMinWidth = { minWidth: `${windowWidth * 0.75 > 300 && windowWidth - 320}px` }
-  const colorSpan = { color: shade === "DEFAULT" ? `var(--${color})` : `var(--${color}-${shade})` }
+    }, []);
+    return width;
+  };
+  const windowWidth = useWidth();
+  const closeDrawerMinWidth = {
+    minWidth: `${windowWidth * 0.75 > 300 && windowWidth - 320}px`,
+  };
+  const colorSpan = {
+    color: shade === "DEFAULT" ? `var(--${color})` : `var(--${color}-${shade})`,
+  };
 
   return (
     <>
@@ -42,9 +46,13 @@ const MobileDrawer = ({
           <div
             onClick={handleToggle}
             style={closeDrawerMinWidth}
-            className={`${classNames.closeDrawer} ${isOpen ? classNames.open : ""}`}
+            className={`${classNames.closeDrawer} ${
+              isOpen ? classNames.open : ""
+            }`}
           ></div>
-          <div className={`${classNames.drawer} ${isOpen ? classNames.open : ""}`}>
+          <div
+            className={`${classNames.drawer} ${isOpen ? classNames.open : ""}`}
+          >
             <div className={classNames.crossIcon} onClick={handleToggle}>
               <Icon name="cross" color={color} shade={shade} />
             </div>
@@ -52,11 +60,13 @@ const MobileDrawer = ({
               <img src={logo} alt="logo" />
             </div>
             <div className={classNames.containerMenu}>
-              {menu.map(({ name, onClick }) => (
-                <span style={colorSpan} onClick={onClick} key={name}>
-                  {name}
-                </span>
-              ))}
+              {menu.map(({ name, isOnlyDesktop, onClick }) =>
+                !isOnlyDesktop && (
+                  <span style={colorSpan} onClick={onClick} key={name}>
+                    {name}
+                  </span>
+                )
+              )}
             </div>
           </div>
         </div>
